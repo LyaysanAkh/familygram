5f7a6ba411616482f6b0133eff6f1273f2f1e959
first_spring_rain <120134977+LyaysanAkh@users.noreply.github.com>
2023-02-06 02:58:36 +0300

    Добавила скролл в комментариях

--- static/comments.js
+++ static/comments.js
@@ -2,14 +2,14 @@ let commentList = document.querySelectorAll('.comment-list');
 let commentField = document.querySelectorAll('.comment-field');
 let commentForm = document.querySelectorAll('.comment-form');
 
-for (let i=0; ;i++) {
+for (let i=0;i<1000000;i++) {
     commentForm[i].onsubmit = function (evt) {
         evt.preventDefault();
-
         let newComment = document.createElement('li');
         newComment.classList.add('user-comment');
-        newComment.textContent = commentField[i].value;
+        newComment.textContent = commentField[i].value; 
         commentField[i].value = '';
         commentList[i].append(newComment);
-}
-}
+        commentList[i].classList.add('scroll-visible');
+    }
+}    
