7464d9f5a959ad550aaa536a8f9ff9a2610a72ca
first_spring_rain <120134977+LyaysanAkh@users.noreply.github.com>
2023-02-06 23:30:24 +0300

    Добавила счетчик длины комментариев

--- static/comments.js
+++ static/comments.js
@@ -1,6 +1,8 @@
 let commentList = document.querySelectorAll('.comment-list');
 let commentField = document.querySelectorAll('.comment-field');
 let commentForm = document.querySelectorAll('.comment-form');
+let button = document.querySelectorAll('.button');
+let charCounter = document.querySelectorAll('.char-counter');
 
 for (let i=0;i<1000000;i++) {
     commentForm[i].onsubmit = function (evt) {
@@ -11,5 +13,17 @@ for (let i=0;i<1000000;i++) {
         commentField[i].value = '';
         commentList[i].append(newComment);
         commentList[i].classList.add('scroll-visible');
+        charCounter[i].textContent = 0;
     }
-}    
+
+    commentField[i].oninput = function() {
+        charCounter[i].textContent = commentField[i].value.length;
+        if (commentField[i].value.length > 100) {
+        charCounter[i].classList.add('warning');
+        button[i].disabled = true;
+        } else {
+        charCounter[i].classList.remove('warning');
+        button[i].disabled = false;
+        }
+    }
+}
